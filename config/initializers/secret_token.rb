c6801c499b454e5e10b4ef46f68041e9a299ae33
Derek Hopper <derek@toutapp.com>
2011-10-19 15:02:59 -0400

    Initial import for personal website.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personal::Application.config.secret_token = 'ecd5f828392fc8b458623e7a0c8a38afe7b76ee728fb7618818a3eefa633fac7ede79b462444ce89632e517079813cda15e69ae7ada8ae21cb43ef8cf3fde0ec'
