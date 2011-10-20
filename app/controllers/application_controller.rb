24e3193bb42f955ba619702534b5e1cb01617b27
Derek Hopper <derek@toutapp.com>
2011-10-20 18:22:20 -0400

    Updating right sidebar and footbar. Preparing content section for views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :daily_reading
+  helper_method :daily_reading, :my_products, :my_code
   
+  # Links to websites I read daily
   def daily_reading
     links = ActiveSupport::OrderedHash.new
     links["ESPN"]          = {:url => "http://www.espn.com", :description => "Scores and NFL blogs, my addiction."}
     links["CNN"]           = {:url => "http://www.cnn.com", :description => "Mostly headlines."}
+    links
+  end
+  
+  # My products
+  def my_products
+    links = ActiveSupport::OrderedHash.new
+    links["BatStatz"] = {:description => "A tool for youth baseball coaches to keep track of their team's statistics. Parents also get a team website for their kids."}
+    links
+  end
+  
+  # Small hacks with link to Github
+  def my_code
+    links = ActiveSupport::OrderedHash.new
+    links["Easy Unfriend"] = {:url => "https://chrome.google.com/webstore/detail/gghpnhoakonhhpjbalfjgmfkejaiidih?ct=author", :github => "https://github.com/djhopper01/Easy-Unfriend", :description => "A Chrome extension that helps you unfriend people on Facebook."}
+    links["Shoutfire"] = {:github => "https://github.com/djhopper01/shoutfire", :description => "An Android application with a MySQL/PHP backend. Users chat with each other, but only see the last 100 messages."}
     links
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def display_links(links)
+    html = ""
+    
+    links.each do |name, info|
+      html << "<li>"
+      if info.has_key? :url
+        html << link_to(name, info[:url])
+      else
+        html << "<span class='important'>#{name}</span>"
+      end
+      html << " <span>#{info[:description]}</span>"
+      if info.has_key? :github
+        html << "<br/>"
+        html << "(#{link_to "Github", info[:github], :class => "faded"})"
+      end
+      html << "</li>"
+    end
+    raw html
+  end
 end
