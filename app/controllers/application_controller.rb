4bf346f00608605ce54f513b7cd4f20505e1277f
Derek Hopper <derek@toutapp.com>
2011-10-20 19:07:32 -0400

    Adding some main content.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def daily_reading
     links = ActiveSupport::OrderedHash.new
     links["ESPN"]          = {:url => "http://www.espn.com", :description => "Scores and NFL blogs, my addiction."}
-    links["CNN"]           = {:url => "http://www.cnn.com", :description => "Mostly headlines."}
+    links["CNN"]           = {:url => "http://www.cnn.com", :description => "Most of the time, I'm reading headlines."}
     links
   end
   
@@ -21,6 +21,7 @@ class ApplicationController < ActionController::Base
   # Small hacks with link to Github
   def my_code
     links = ActiveSupport::OrderedHash.new
+    links["Beer for Textbooks"] = {:url => "http://www.beerfortextbooks.com", :description => "A website I built during a Startup Weekend. The title says it all."}
     links["Easy Unfriend"] = {:url => "https://chrome.google.com/webstore/detail/gghpnhoakonhhpjbalfjgmfkejaiidih?ct=author", :github => "https://github.com/djhopper01/Easy-Unfriend", :description => "A Chrome extension that helps you unfriend people on Facebook."}
     links["Shoutfire"] = {:github => "https://github.com/djhopper01/shoutfire", :description => "An Android application with a MySQL/PHP backend. Users chat with each other, but only see the last 100 messages."}
     links
