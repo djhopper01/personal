d18e0ae6f89a280613f1cd4877e51c8d8778e8af
Derek Hopper <derek@toutapp.com>
2011-10-20 17:34:32 -0400

    Working above the fold.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :daily_reading
+  
+  def daily_reading
+    links = ActiveSupport::OrderedHash.new
+    links["ESPN"]          = {:url => "http://www.espn.com", :description => "Scores and NFL blogs, my addiction."}
+    links["CNN"]           = {:url => "http://www.cnn.com", :description => "Mostly headlines."}
+    links
+  end
 end
